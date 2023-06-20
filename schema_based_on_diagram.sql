ce033c12c2edb93bd58cd3bcd1ec6f95e1118d03
Isabella Otoo <isabellaotoo25@gmail.com>
2023-06-20 13:48:45 +0000

    Update schema_based_on_diagram.sql

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,42 +1,48 @@
 CREATE TABLE patients (
-id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
-name VARCHAR (255),
-date_of_birth date,
-PRIMARY KEY (id);
-);
-
-CREATE TABLE invoices (
-id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
-total_amount decimal,
-generated_at timestamp,
-payed_at timestamp,
-medical_history_id INT REFERENCES medical_histories(id),
-PRIMARY KEY (id);
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+  name VARCHAR(255),
+  date_of_birth DATE,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE medical_histories (
-id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
-admitted_at timestamp,
-patient_id INT REFERENCES patients(id),
-status VARCHAR
-PRIMARY KEY (id);
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+  admitted_at TIMESTAMP,
+  patient_id INT REFERENCES patients(id),
+  status VARCHAR,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE invoices (
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+  total_amount DECIMAL,
+  generated_at TIMESTAMP,
+  paid_at TIMESTAMP,
+  medical_history_id INT REFERENCES medical_histories(id),
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE treatments (
-id INT REFERENCES medical_histories(id),
-type VARCHAR,
-name VARCHAR
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+  type VARCHAR,
+  name VARCHAR,
+  PRIMARY KEY (id),
+  FOREIGN KEY (id) REFERENCES medical_histories(id)
 );
 
 CREATE TABLE invoice_items (
-id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
-unit_price decimal,
-quantity INT,
-total_price decimal,
-invoice_id INT REFERENCES invoices(id),
-treatment_id  INT REFERENCES treatments(id),
-PRIMARY KEY (id);
+  id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+  unit_price DECIMAL,
+  quantity INT,
+  total_price DECIMAL,
+  invoice_id INT REFERENCES invoices(id),
+  treatment_id INT REFERENCES treatments(id),
+  PRIMARY KEY (id)
 );
+
+
+
+
 
 
 
